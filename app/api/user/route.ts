373337693bf834fa7f1605fd2ee5b63c826d08ce
kai kusakari <kaikusakari@kais-MacBook-Air.local>
2025-03-22 01:48:24 +0900

    add session api

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,8 +1,10 @@
+import { auth } from "@/auth";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient()
-export async function GET() {
-    const userlist = await prisma.user.findMany({})
-    return NextResponse.json(userlist);
-}
+export const GET = auth( async function GET(req) {
+    if (!req.auth) return NextResponse.json({ message: "Not authenticated" }, { status: 401 })
+        const userlist = await prisma.user.findMany({})
+        return NextResponse.json(userlist);
+})
