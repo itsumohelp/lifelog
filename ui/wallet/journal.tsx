0d58781a56a5e8382e076eca3d599c4c22f9e341
kai kusakari <kaikusakari@kais-MacBook-Air.local>
2025-04-13 17:40:49 +0900

    add come api

--- ui/wallet/journal.tsx
+++ ui/wallet/journal.tsx
@@ -1,44 +1,68 @@
-"use client"
-import { useEffect, useState } from "react";
+"use client";
+import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
 
-export default function Journal(props:{ wallet: { id: string | undefined; }; }) {
-  interface Come {
-      id: string;
-      amount: number;
-      createdAt: string;
-  }
+interface Wallet {
+  id: string;
+  name: string;
+  balance: number;
+}
+interface Come {
+  id: string;
+  amount: number;
+  createdAt: string;
+}
 
-  const [comeData, setComeData] = useState<Come[] | null>(null)
-  const [isLoading, setLoading] = useState(true)
+export default function Journal(props: { wallet: Wallet; come: Come[]; setWallet: any; journalUpdate: any }) {
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`/api/wallet/${props.wallet.id}`);
-      const resData = await response.json();
-      setComeData(resData);
-      setLoading(false);
-    };
-    fetchData();
-  }, [])
- 
-  if (isLoading) return <p>Loading...</p>
-  if (!comeData) return (<div>No journal data.<br/></div>)
+    props.journalUpdate();
+  }, []);
 
-return (
+  const deleteRow = (id: string, amount: number) => {
+  fetch(`/api/come/${id}`, { method: "DELETE" })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      props.setWallet((amount * -1) as number);
+      props.journalUpdate()
+    })
+    .catch((error: any) => {
+      console.error("エラーが発生しました", error);
+    });
+  };
+  return (
     <div>
-          <table className="border-separate border-spacing-2">
-            <thead>
-              <tr><th>金額</th><th>登録時間</th></tr>
-            </thead>
-            <tbody>
-      {comeData.map((item:any, index) => (
-              <tr key={index}>
-                <td className="text-right">{item.amount}</td>
-                <td>{item.createdAt}</td>
-              </tr>
-      ))}
+      <table className="border-separate border-spacing-2">
+        <thead>
+          <tr>
+            <th>金額</th>
+            <th>登録時間</th>
+            <th>削除</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.come.map((item: Come, index) => (
+            <tr key={index}>
+              <td className="text-right">{item.amount}</td>
+              <td>{item.createdAt}</td>
+              <td>
+                <button
+                  type="button"
+                  className="rounded bg-gray-200 p-2 transition-colors hover:bg-gray-300"
+                  onClick={() => deleteRow(item.id, item.amount)}
+                >
+                  削除
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
-        </table>
+      </table>
     </div>
-  )
+  );
+}
+
+function setWallet(arg0: (prev: Wallet) => { balance: number }) {
+  throw new Error("Function not implemented.");
 }
