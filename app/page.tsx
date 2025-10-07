4d79ede1853fdc39b4a778e87dfb817534727856
kai kusakari <kaikusakari@kais-MacBook-Air.local>
2025-10-07 22:13:07 +0900

    change ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
 import UserButton from "@/components/user-button";
 import WalletList from "@/ui/wallet/list";
 import {SessionProvider} from "next-auth/react";
+import LogoPage from "./logo";
 
 export default function Home() {
     return (
         <div>
             <div className="grid grid-cols-5 gap-4 p-1">
                 <div className="col-span-2">
-                    <h1 className="text-3xl font-bold">ITSUMO</h1>
+                    <LogoPage />
                 </div>
                 <div className="col-span-3"  ><UserButton /></div>
             </div>
             <SessionProvider>
                 <WalletList />
             </SessionProvider>
-        </div>
+        </div >
     );
 }
