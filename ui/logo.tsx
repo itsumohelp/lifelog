ad527529b1f623e8b9816504dfa07dc655308fce
kai kusakari <kaikusakari@kais-MacBook-Air.local>
2025-10-07 22:28:18 +0900

    change ui

---
+++ ui/logo.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Page() {
+    function getVer() {
+        alert("4DFB726D-A438-4B0A-B3F6-9A7FBB912F65");
+    }
+
+    return <h1 className="text-3xl font-bold" onClick={getVer}>ITSUMO</h1>;
+}
